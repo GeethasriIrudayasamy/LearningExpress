201e375b8207f5933de8aacc4eaacc9d5c3a3f53
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2023-02-20 21:06:43 +0530

    First step to express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("first commit");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("First middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    res.send("<html><h1>Hello from Express!!!</h1></html>");
+});
+
+app.listen(4000);
+
+// const server = http.createServer(app);
+// server.listen(4000);
