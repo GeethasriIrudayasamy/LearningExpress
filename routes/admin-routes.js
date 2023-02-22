620d19211ca74d53acdd63c46d46799b386e5a37
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2023-02-22 21:01:44 +0530

    a minor change

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use("/add-product", (req, res, next) => {
     res.send(
-        '<html><form action="/product" method="POST"><label>Product Name:</label><input type="text" name="product"/><br/><br/><label>Size:</label><input type="text" name="size"/><br/><br/><button type="submit">Add Product</button></form></html>'
+        '<html><form action="/admin/product" method="POST"><label>Product Name:</label><input type="text" name="product"/><br/><br/><label>Size:</label><input type="text" name="size"/><br/><br/><button type="submit">Add Product</button></form></html>'
     );
 });
 
